52b3e5cc70dea8c41bb13179e90eb9fe7a1b7e7e
Luqman Design <lzazi24@student.aau.dk>
2025-10-24 13:06:27 +0200

    excelreader first version
    fra russerens video med dependencies

---
+++ src/main/java/group10/Main.java
@@ -0,0 +1,17 @@
+package group10;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+public class Main {
+public static void main(String[] args) throws InvalidFormatException, IOException {
+		File excelFile = Paths.get("").resolve("sample_employee_data.xlsx").toFile();
+		Excelreader reader = new Excelreader(excelFile);
+		reader.readFromExcelFile();
+	}
+}
+    
+
