6ad24ba12409aa3744c47cff11ce7c43dffb5f73
Luqman Design <lzazi24@student.aau.dk>
2025-11-03 14:24:31 +0100

    localdatetime removed.

--- src/main/java/group10/excel/RealisedCapacity.java
+++ src/main/java/group10/excel/RealisedCapacity.java
@@ -1,26 +1,19 @@
 package group10.excel;
 
-import java.time.LocalDate;
 
 public class RealisedCapacity {
   private int palletAmount;
-  private LocalDate date;
   private Temperature temperature;
   private Warehouse warehouse;
 
-  public RealisedCapacity(int palletAmount, LocalDate date, Temperature temperature, Warehouse warehouse) {
+  public RealisedCapacity(int palletAmount, Temperature temperature, Warehouse warehouse) {
     this.palletAmount = palletAmount;
-    this.date = date; // Behøves dette eller skal vi bruge datoen?
     this.temperature = temperature;
     this.warehouse = warehouse;
   }
 
   public int getPalletAmount() {
     return this.palletAmount;
-  }
-
-  public LocalDate getDate() {
-    return this.date;
   }
 
   public Temperature getTemperature() {
